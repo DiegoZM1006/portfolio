f42b72fc25b1de3ac71e97ccafe3c909dfbfe028
DiegoZM1006 <1109184718@u.icesi.edu.co>
2025-11-01 22:33:47 -0500

    ci: fix site url in astro.config.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://diegomueses.dev', // Cambia esto por tu URL de producción
+  site: 'https://diegozm.netlify.app', // Cambia esto por tu URL de producción
   
   vite: {
     plugins: [tailwindcss()]
